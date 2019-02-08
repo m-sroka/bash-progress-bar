47daaa08cc0345d05322970db1d3c55ade5b26d9
Maciej Sroka <maciej.sroka2@gmail.com>
2019-02-08 22:55:06 +0000

    Better samples

--- sample_usages.sh
+++ sample_usages.sh
@@ -2,14 +2,14 @@
 
 
 declare -a arr=(
-'progressbar 70'
-'progressbar 70 -s 40'
-'progressbar 50 -a'
- 'progressbar 150'
-'progressbar 150 -l'
-'progressbar 150 -c blue'
-'progressbar 80 -c brown'
-'progressbar 70 -w 😼'
+'progressbar 70%'
+'progressbar 70% -s 40'
+'progressbar 50% -a'
+ 'progressbar 150%'
+'progressbar 150% -l'
+'progressbar 150% -c blue'
+'progressbar 80% -c brown'
+'progressbar 70% -w 😼'
 )
 
 
