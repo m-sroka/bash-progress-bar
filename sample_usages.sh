5e1ad57d5629e8a4a9c86126e9043de94507e50d
Maciej Sroka <maciej.sroka2@gmail.com>
2019-02-08 22:53:21 +0000

    Refactoring

--- sample_usages.sh
+++ sample_usages.sh
@@ -2,46 +2,21 @@
 
 
 declare -a arr=(
-'progress-bar 10'
-'progress-bar 10 -s 80'
-'progress-bar 50 -a'
- 'progress-bar 150'
-'progress-bar 150 -l'
-'progress-bar 150 -c blue'
-'progress-bar 80 -c brown'
-'progress-bar 70 -w 😼'
+'progressbar 70'
+'progressbar 70 -s 40'
+'progressbar 50 -a'
+ 'progressbar 150'
+'progressbar 150 -l'
+'progressbar 150 -c blue'
+'progressbar 80 -c brown'
+'progressbar 70 -w 😼'
 )
 
 
 for cmd in "${arr[@]}"
 do
   echo $cmd
-  eval "./${cmd}"
   echo
+  eval "./${cmd}"
+  printf '\n\n\n\n'
 done
-exit 0
-echo 'progress_bar 10'
-./progress_bar.sh 10; echo; echo
-
-echo 'progress_bar 10 -s 80'
-./progress_bar.sh 10 -s 80; echo; echo
-
-echo 'progress_bar 50 -a'
-./progress_bar.sh 50 -a; echo; echo
-
-echo 'progress_bar 150'
-./progress_bar.sh 150; echo; echo
-
-echo 'progress_bar 150 -l'
-./progress_bar.sh 150 -l; echo; echo
-
-echo 'progress_bar 150 -c blue'
-./progress_bar.sh 150 -c blue; echo; echo
-
-echo 'progress_bar 80 -c brown'
-./progress_bar.sh 150 -c brown; echo; echo
-
-echo 'progress_bar 70 -w 😼'
-./progress_bar.sh 70 -w 😼; echo; echo
-
-
