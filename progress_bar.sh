eb064769021c306266c8123788bc2aa6ebbbe48b
Maciej Sroka <maciej.sroka2@gmail.com>
2019-02-08 22:15:04 +0000

    Colors

--- progress_bar.sh
+++ progress_bar.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 
+getColor() {
+  case "$1" in
+    black) color="0;30m";;
+    red) color="1;31m";;
+    green) color="1;32m";;
+    blue) color="1;34m";;
+    brown) color="1;33m";;
+    yellow) color="1;33m";;
+    white) color="1;37m";;
+  esac
+  echo "\033[$color"
+}
+DEFAULT_COLOR=`getColor 'white'`
+
 
 # PARSE ARGS
 
-print_help() {
+printHelp() {
   echo
   echo 'usage: progress-bar [VALUE], VALUE percentage value, e.g. 10%, 50%, 100%, 110%'
   echo
   echo '  -l long output - allow more than 100%'
   echo '  -s [NUM], NUM number of ■ showed when value is 100 (default: 10)'
-  echo '  -c [CHARACTER], CHARACTER characted that is being shown as progress bar tile (default: ■)'
+  echo '  -w [CHARACTER], CHARACTER characted that is being shown as progress bar tile (default: ■)'
   echo '  -a append numeric value after progress bar'
+  echo '  -c [COLOR] black|red|green|blue|brown|yellow|white'
 }
 
 # DEFAULT CONFIG PARAMETERS
@@ -19,12 +34,14 @@ allow_more_than_max=false
 character=■
 granularity=10
 append_numeric_after_progressbar=false
+color=$DEFAULT_COLOR
 
-while getopts 'als:c:h' OPT ${@:2}; do
+while getopts 'als:c:w:h' OPT ${@:2}; do
   case "$OPT" in
     "l") allow_more_than_max=true ;;
     "s") granularity=$OPTARG ;;
-    "c") character=$OPTARG ;;
+    "w") character=$OPTARG ;;
+    "c") color=$(getColor $OPTARG) ;;
     "a") append_numeric_after_progressbar=true ;;
     "h") print_help; exit 1 ;;
     "?") print_help >&2; exit 1 ;;
@@ -38,10 +55,13 @@ value=$(echo $1 | tr -d %)
 let step_size=100/$granularity
 let num_of_progress_items=$value/$step_size
 
+
+printf ${color}
+
 printf '|'
 for i in $(seq 0 $granularity)
 do
-  [[ $i -le $num_of_progress_items ]] && printf $character || printf ' '
+  [[ $i -le $num_of_progress_items ]] && printf "$character" || printf ' '
 done  
 
 printf '|'
